900dbec7b0538b2bce763d8fedb8d23d60235b4d
Rata13NS <161605200+Rata13NS@users.noreply.github.com>
2024-03-14 18:37:03 +0200

    Update TicTacToe.js

--- TicTacToe.js
+++ TicTacToe.js
@@ -5,6 +5,7 @@ let boxes = [];
 let userContor = 1;
 let buttonContor = 0;
 let neither = 0;
+var stopfunction = true;
 
 function createGrid() {
     for (let i = 0; i < 3; i++) {
@@ -23,6 +24,7 @@ function createGrid() {
 function game() {
     boxes.forEach(function(box, index) {
         box.addEventListener('click', function() {
+            if (stopfunction === false) return;
             if (userContor === 1) {
               box.textContent = 'X';
               userContor = 2;
@@ -40,27 +42,35 @@ function statusGame() {
     if (boxes[0].textContent === boxes[1].textContent && boxes[1].textContent === boxes[2].textContent && boxes[1].textContent != '') {
         showWinner();
         restartButton();
+        stopfunction = false;
     } else if (boxes[3].textContent === boxes[4].textContent && boxes[4].textContent === boxes[5].textContent && boxes[4].textContent != '') {
         showWinner();
         restartButton();
+        stopfunction = flase;
     } else if (boxes[6].textContent === boxes[7].textContent && boxes[7].textContent === boxes[8].textContent && boxes[7].textContent != '') {
         showWinner();
         restartButton();
+        stopfunction = false;
     } else if (boxes[0].textContent === boxes[3].textContent && boxes[3].textContent === boxes[6].textContent && boxes[3].textContent != '') {
         showWinner();
         restartButton();
+        stopfunction = false;
     } else if (boxes[1].textContent === boxes[4].textContent && boxes[4].textContent === boxes[7].textContent && boxes[4].textContent != '') {
         showWinner();
         restartButton();
+        stopfunction = false;
     } else if (boxes[2].textContent === boxes[5].textContent && boxes[5].textContent === boxes[8].textContent && boxes[5].textContent != '') {
         showWinner();
         restartButton();
+        stopfunction = false;
     } else if (boxes[0].textContent === boxes[4].textContent && boxes[4].textContent === boxes[8].textContent && boxes[4].textContent != '') {
         showWinner();
         restartButton();
+        stopfunction = false;
     } else if (boxes[2].textContent === boxes[4].textContent && boxes[4].textContent === boxes[6].textContent && boxes[4].textContent != '') {
         showWinner();
         restartButton();
+        stopfunction = false;
     } else if (neither === 9) {
         winnerPlayer.innerHTML = "Neither one won! Try a new game!";
         restartButton();
@@ -79,6 +89,7 @@ function restartButton() {
         winnerPlayer.innerHTML = '';
         boxes = [];
         userContor = 1;
+        stopfunction = true;
         createGrid();
         game();
     });
